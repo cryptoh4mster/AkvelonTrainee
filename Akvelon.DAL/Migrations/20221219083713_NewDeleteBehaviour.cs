4df1af8763e31e45d1ad27e30bd9cf84dc8ddf05
Nikita Zaikin <l1ve_to_w1n@mail.ru>
2022-12-19 14:51:32 +0600

    Add new delete behaviour

---
+++ Akvelon.DAL/Migrations/20221219083713_NewDeleteBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Akvelon.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewDeleteBehaviour : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Projects",
+                table: "Tasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Projects",
+                table: "Tasks",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Projects",
+                table: "Tasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Projects",
+                table: "Tasks",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id");
+        }
+    }
+}
